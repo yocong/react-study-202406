8a260d3f949a63f0eae7e6ccf5570e2b5594da9a
yocong <rnfaos77@naver.com>
2024-06-12 12:25:01 +0900

    # 이벤트 바인딩

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -5,31 +5,22 @@ import Card from '../UI/Card';
 
 const ExpenseItem = ({ date, title, price: exPrice }) => {
 
-  // console.log('props: ', aaa);
-
-  // 변수 선언
-  // const expenseDate = date;
-  // const expenseTitle = title;
-  // const expensePrice = exPrice;
-
   // 함수 선언
-
-  // 1자리 숫자를 2자리수로 변환하는 함수
-  const make2digit = (text) => {
-    return text.toString().padStart(2, '0');
-  };
-
-  const makeFormattedDate = () => {
-
-    const year = date.getFullYear();
-    const month = date.getMonth();
-    const day = date.getDate();
-
-    return `${year}년 ${make2digit(month)}월 ${make2digit(day)}일`;
-  };
 
   // 원화 표기법으로 변환
   const formattedPrice = new Intl.NumberFormat('ko-KR').format(exPrice);
+
+ // 이벤트 핸들러 선언
+ const clickHandler = e => {
+  console.log('클릭함!');
+  console.log(e.target.previousElementSibling.firstElementChild.textContent);
+
+  // 이벤트 핸들러는 버튼이 그려진 이후기 때문에 다 잡아올 수 있음
+  // const $price = document.querySelectorAll('.expense-item__price');
+  // console.log($price);
+};
+
+  
 
   return (
       <Card className='expense-item'>
@@ -38,6 +29,8 @@ const ExpenseItem = ({ date, title, price: exPrice }) => {
           <h2>{title}</h2>
           <div className='expense-item__price'>{formattedPrice}원</div>
         </div>
+        <button id='btn' onClick={clickHandler}>버튼1</button>
+        <button id='btn2' onMouseOver={e => alert('하하!')}>버튼2</button>
       </Card>
   )
 }
