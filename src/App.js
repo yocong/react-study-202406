03821dc8c75d4239537e13ef8a59604bb895a405
yocong <rnfaos77@naver.com>
2024-06-12 11:14:09 +0900

    # props 기초

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import './App.css';
-import React from 'react';
-import ExpenseItem from './components/expenses/ExpenseItem';
+import "./App.css";
+import React from "react";
+import ExpenseItem from "./components/expenses/ExpenseItem";
 
 const App = () => {
-
   return (
-  <>
-    <ExpenseItem />
-  </>
+    <>
+      <ExpenseItem
+        title={"치킨냠냠"}
+        price={30000}
+        date={new Date(2024, 6, 3)}
+      />
+      <ExpenseItem
+        title={"족발야미"}
+        price={40000}
+        date={new Date(2024, 6, 7)}
+      />
+      <ExpenseItem
+        title={"피자꺼억"}
+        price={25000}
+        date={new Date(2024, 6, 12)}
+      />
+    </>
   );
-}
+};
 
 export default App;
