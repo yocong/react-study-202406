8073f9269f9f96fa9f09a5260bfa52add98aae2b
yocong <rnfaos77@naver.com>
2024-06-13 12:16:20 +0900

    # drilling 연습

--- src/components/expenses/ExpenseList.js
+++ src/components/expenses/ExpenseList.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import ExpenseItem from './ExpenseItem'
+import ExpenseFilter from './ExpenseFilter'
 
 const ExpenseList = ({ expenses }) => {
+
+  const onFilterChange = (filteredYear) => {
+    // ExpenseFilter에 있는 선택된 연도값을 여기서 출력!
+    console.log(filteredYear);
+  }
+
   return (
     <div className='expenses'>
+
+      <ExpenseFilter onChangeFilter={onFilterChange}/>
+
       <ExpenseItem
         title={expenses[0].title}
         price={expenses[0].price}
