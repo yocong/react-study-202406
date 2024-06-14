5b23501bca650bf822d84a1207d4245fe86214b3
yocong <rnfaos77@naver.com>
2024-06-14 12:47:50 +0900

    목표 삭제하기

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React, { useState } from "react";
-import CourseInput from "./components/CourseGoals/CourseInput";
-import CourseList from "./components/CourseGoals/CourseList";
-import "./App.css";
+import React, { useState } from 'react';
+import CourseInput from './components/CourseGoals/CourseInput';
+import CourseList from './components/CourseGoals/CourseList';
+import './App.css';
 
 // 기본 더미 데이터
 const DUMMY_DATA = [
   {
-    id: "g1",
-    text: "리액트 컴포넌트 스타일링 마스터하기",
+    id: 'g1',
+    text: '리액트 컴포넌트 스타일링 마스터하기'
   },
   {
-    id: "g2",
-    text: "UI/UX 프로그래밍 쌉고수되기",
+    id: 'g2',
+    text: 'UI/UX 프로그래밍 쌉고수되기'
   },
 ];
 
@@ -19,23 +19,43 @@ const App = () => {
 
   const [goals, setGoals] = useState(DUMMY_DATA);
 
-  // CourseInput에 내려보낼 함수
-  const addGoalsHandler = (userInput) => {
-    goals.push(userInput);
+  // CourseInput에게 전달할 함수
+  const addGoalHandler = (goalObject) => {
+    setGoals([...goals, goalObject]);
+  };
 
-    // setter로 변경감지 하려면 새로운 배열을 넣어주어야함
-    setGoals([...goals, userInput]);
+  // CouseItem에게 전달할 함수
+  const deleteGoalHandler = (id) => {
 
-    console.log(goals);
+    // 1. for문으로 삭제
+    // let index = -1;
+    // for (let i = 0; i < goals.length; i++) {
+    //   if (goals[i].id === id) {
+    //     index = i;
+    //     break;
+    //   }
+    // }
+
+    // goals.splice(index, 1);
+    
+    // setGoals([...goals]);
+
+    // 2. findIndex
+    // goals.splice(goals.findIndex(g => g.id === id), 1);
+
+    // 3. filter, 일치하지 않은 애들은 필터 => 즉, 일치하는 애들만 선택되어서 삭제
+    setGoals(goals.filter(g => g.id != id));
+
+
   };
 
   return (
     <div>
       <section id="goal-form">
-        <CourseInput onAdd={addGoalsHandler}/>
+        <CourseInput onAdd={addGoalHandler} />
       </section>
       <section id="goals">
-        <CourseList items={goals} />
+        <CourseList items={goals} onDelete={deleteGoalHandler} />
       </section>
     </div>
   );
--- src/components/CourseGoals/CourseItem.js
+++ src/components/CourseGoals/CourseItem.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './CourseItem.css';
 
-const CourseItem = ({ item }) => {
-  return <li className="goal-item">{item.text}</li>;
+const CourseItem = ({ item, onDelete }) => {
+
+  const deleteHandler = e => {
+    console.log('삭제 해줘');
+    // 여기서 App.js에게 삭제 대상의 id를 전달
+    onDelete(item.id);
+  }
+
+  return <li className="goal-item" onClick={deleteHandler}>{item.text}</li>;
 };
 
 export default CourseItem;
