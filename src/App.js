304045f4fb3499c7738c0c3a6350a1ad11df9b89
yocong <rnfaos77@naver.com>
2024-06-17 12:40:21 +0900

    # useRef로 DOM 조작 가능 (직접 접근 안해도됨)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import './App.css';
 import AddUsers from './components/Users/AddUsers';
 import UserList from './components/Users/UserList';
@@ -10,7 +10,14 @@ const App = () => {
     // 회원들이 저장될 배열
     const [userList, setUserList] = useState([]);
 
+    const count = useRef(1);
+    console.log('count: ', count);
+
     const addUserHandler = user => {
+
+        count.current++;
+        console.log('count.current: ', count.current);
+
         console.log(user);
         setUserList(prev => [
             ...prev, // 기존 회원 복사해서 오고,
