bdb45030f40685774bf04edb3958218e445fde76
yocong <rnfaos77@naver.com>
2024-06-17 10:23:06 +0900

    css 모듈

--- src/components/CourseGoals/CourseInput.js
+++ src/components/CourseGoals/CourseInput.js
@@ -1,30 +1,20 @@
-import React, {useState} from 'react';
-import './CourseInput.css';
+import React, { useState } from 'react';
+import styles from './CourseInput.module.css';
 import Button from '../UI/Button';
 
 const CourseInput = ({ onAdd }) => {
 
-  // 목표 input에 입력한 값
+  // console.log('s: ', styles);
+
+  const { invalid, "form-control": formControl } = styles;
+
+  // 목표 인풋에 입력한 값
   const [enteredText, setEnteredText] = useState('');
 
   // 입력값 검증을 통과했는지 여부를 상태관리
   const [isValid, setIsValid] = useState(true);
 
-  // 입력값 바뀔 때 이벤트 핸들러
-  const goalAddHandler = e => {
-
-    const inputValue = e.target.value;
-
-    // 입력값 검증
-    if (inputValue.trim().length > 0) {
-      setIsValid(true);
-    }
-    setEnteredText(inputValue);
-
-  };
-
-  // 폼 전송 이벤트 핸들러
-  const formSubmitHandler = e => {
+  const formSubmitHandler = (e) => {
     e.preventDefault();
 
     if (enteredText.trim().length === 0) {
@@ -34,24 +24,34 @@ const CourseInput = ({ onAdd }) => {
 
     const newGoalObject = {
       id: Math.random().toString(),
-      text: enteredText
+      text: enteredText,
     };
-    console.log(newGoalObject);
+    // console.log(newGoalObject);
 
-    // 부모 컴포넌트로 데이터 보내기
     onAdd(newGoalObject);
 
-    // input 비우기
     setEnteredText('');
-  }
+  };
+
+  const goalChangeHandler = (e) => {
+
+    const inputValue = e.target.value;
+
+    // 입력값 검증
+    if (inputValue.trim().length > 0) {
+      setIsValid(true);
+    }
+    setEnteredText(inputValue);
+  };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${isValid ? '' : 'invalid'}`}>
+      <div className={`${formControl} ${!isValid ? invalid : ''}`}>
         <label>나의 목표</label>
-        <input type="text"
-        onChange={goalAddHandler}
-        value={enteredText}
+        <input
+          type="text"
+          onChange={goalChangeHandler}
+          value={enteredText}
         />
       </div>
       <Button type="submit">목표 추가하기</Button>
