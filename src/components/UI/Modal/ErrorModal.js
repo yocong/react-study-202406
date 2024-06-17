a49824288f0e1bba1041704c25eea52e5ce5a0d1
yocong <rnfaos77@naver.com>
2024-06-17 11:55:59 +0900

    # portal

--- src/components/UI/Modal/ErrorModal.js
+++ src/components/UI/Modal/ErrorModal.js
@@ -1,25 +1,52 @@
 import React from 'react';
+// porta을 사용하기 위한 불러오기
+import ReactDOM from 'react-dom';
 
 import Card from '../Card';
 import Button from '../Button';
 import styles from './ErrorModal.module.css';
 
-const ErrorModal = ({title, message, onClose}) => {
+const BackDrop = ({ onClose }) => {
+  return (
+    <div
+      className={styles.backdrop}
+      onClick={onClose}
+    ></div>
+  );
+};
+
+const ModalOverlay = ({ title, message, onClose }) => {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{title}</h2>
+      </header>
+      <div className={styles.content}>
+        <p>{message}</p>
+      </div>
+      <footer className={styles.actions}>
+        <Button onClick={onClose}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
+
+const ErrorModal = ({ title, message, onClose }) => {
   return (
     <>
-      <div className={styles.backdrop} onClick={onClose}></div>
+      {
+        ReactDOM.createPortal(
+          <BackDrop onClose={onClose} />
+          , document.getElementById('backdrop-root')
+        )
+      }
 
-      <Card className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{title}</h2>
-        </header>
-        <div className={styles.content}>
-          <p>{message}</p>
-        </div>
-        <footer className={styles.actions}>
-          <Button onClick={onClose}>Okay</Button>
-        </footer>
-      </Card>
+{
+        ReactDOM.createPortal(
+          <ModalOverlay title={title} message={message} onClose={onClose} />
+          , document.getElementById('overay-root')
+        )
+      }
     </>
   );
 };
