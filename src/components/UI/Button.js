d6984e28eb1a06301f550180239e0ea01c13a275
yocong <rnfaos77@naver.com>
2024-06-17 09:51:52 +0900

    styled component로 변경

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,12 +1,49 @@
-import React from 'react';
-import './Button.css';
+import React from "react";
+// import './Button.css';
+import styled from "styled-components";
 
-const Button = ({ type, onClick, children}) => {
-  return (
-    <button type={type} className="button" onClick={onClick}>
-      {children}
-    </button>
-  );
-};
+// 스타일된 컴포넌트
+// 클래스명이 겹치지 않게됨 (많이 쓰이는 .btn , .button)
+/*
+  .fruit {
+  
+    &:hover {
+    
+    }
+    & > .apple {
+
+    }
+  }
+*/
+const Button = styled.button`
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #8b005d;
+  color: white;
+  background: #8b005d;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: pointer;
+
+  
+  &:focus {
+    outline: none;
+  }
+  
+  &:hover,
+  &:active {
+  background: #ac0e77;
+  border-color: #ac0e77;
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+}
+`;
+
+// 컴포넌트
+// const Button = ({ type, onClick, children}) => {
+//   return (
+//     <button type={type} className="button" onClick={onClick}>
+//       {children}
+//     </button>
+//   );
+// };
 
 export default Button;
