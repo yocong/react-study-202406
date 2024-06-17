35f0038e2aa10e03075096d98dc4913072dd7409
yocong <rnfaos77@naver.com>
2024-06-17 10:51:54 +0900

    # 수정

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styles from './Button.module.css';
 
-컴포넌트
 const Button = ({ type, onClick, children}) => {
   return (
     <button type={type} className={styles.button} onClick={onClick}>
