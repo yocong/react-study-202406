6244fb36af2d9e73a5b71023deea1c80bbe0933e
yocong <rnfaos77@naver.com>
2024-06-17 09:27:45 +0900

    # 동적 인라인 스타일 -> 동적 클래스로 설정변경

--- src/components/CourseGoals/CourseInput.js
+++ src/components/CourseGoals/CourseInput.js
@@ -47,15 +47,11 @@ const CourseInput = ({ onAdd }) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={`form-control ${isValid ? '' : 'invalid'}`}>
         <label>나의 목표</label>
         <input type="text"
         onChange={goalAddHandler}
         value={enteredText}
-        style={{
-          backgroundColor: isValid ? 'transparent' : 'salmon',
-          borderColor: isValid ? 'black' : 'red',
-        }}
         />
       </div>
       <Button type="submit">목표 추가하기</Button>
