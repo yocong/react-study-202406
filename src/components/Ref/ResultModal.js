18077772e6bdbf8a7c6946184c3ca4f01d32ac3a
yocong <rnfaos77@naver.com>
2024-06-20 12:32:09 +0900

    # 점수

--- src/components/Ref/ResultModal.js
+++ src/components/Ref/ResultModal.js
@@ -8,9 +8,12 @@ const ResultModalComponent = ({onReset, targetTime, remainingTime}, ref) => {
   // 시간 포맷팅
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
 
+  // 점수
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
+
   return (
     <dialog ref={ref} className="result-modal">
-      {isLost && <h2>Your lost!</h2>}
+      {isLost ? <h2>Your lost!</h2> : <h2>Your Score: {score}</h2>}
       <p>The target time was <strong>{targetTime} seconds.</strong></p>
       <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
       <form method="dialog">
