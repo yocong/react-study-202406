b2c70617e21d9632b50a9623c5f94abadbba3c20
yocong <rnfaos77@naver.com>
2024-06-21 11:59:55 +0900

    # 장바구니에 보낼 객체 데이터 만들기

--- src/components/Food/Meals/MealItem/MealItem.js
+++ src/components/Food/Meals/MealItem/MealItem.js
@@ -8,6 +8,19 @@ const MealItem = ({id, price, description, name}) => {
 
   const formatPrice = new Intl.NumberFormat("ko-KR").format(price);
 
+  // MealItemForm에서 선택한 수량값을 끌어올려줄 함수
+  const addToCartHandler = (amount) => {
+    
+    // 장바구니에 보낼 객체
+    const item = {
+      id: id,
+      name: name,
+      amount: +amount,
+      price: price
+    };
+    console.log('item: ', item);
+  };
+
   return (
     <li className={meal}>
       <div>
@@ -16,7 +29,7 @@ const MealItem = ({id, price, description, name}) => {
         <div className={priceStyle}>{formatPrice}원</div>
       </div>
       <div>
-        <MealItemForm id={id} />
+          <MealItemForm id={id} onAddToCart={addToCartHandler}/>
       </div>
     </li>
   );
