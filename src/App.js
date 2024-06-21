4c0c914ccb705a2f4202e00943c8645da8d4e420
yocong <rnfaos77@naver.com>
2024-06-21 09:49:51 +0900

    # Meals 관련 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import "./App.css";
 import Player from "./components/Ref/Player";
 import TimerChallenge from "./components/Ref/TimeChallenge";
 import Header from "./components/Food/Layout/Header";
+import Meals from "./components/Food/Meals/Meals";
 
 const App = () => {
   return (
     <>
       <Header />
-      
+      <div id="main">
+        <Meals />
+      </div>
     </>
   );
 };
