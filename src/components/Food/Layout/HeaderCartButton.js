668619d477b7bb201bf81a1dc1302928ce120821
yocong <rnfaos77@naver.com>
2024-06-24 12:41:37 +0900

    # 담기 버튼 눌렀을 때 장바구니 버튼에 애니메이션 생성

--- src/components/Food/Layout/HeaderCartButton.js
+++ src/components/Food/Layout/HeaderCartButton.js
@@ -1,23 +1,42 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartIcon from "./CartIcon";
 import styles from "./HeaderCartButton.module.scss";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = ({ onShow }) => {
 
-  const { totalSelect, cartItems  } = useContext(CartContext);
+  
+  
+  // bump 애니메이션을 제어하는 상태변수
+  const [isBump, setIsBump] = useState(false);
+  
+  // 장바구니 배열 가져오기 (cartItems)
+  const { totalSelect, cartItems } = useContext(CartContext);
 
-  // const numberOfCart = cartItems.reduce((accum, current) => accum + current.amount, 0);
+  const numberOfCart = cartItems.reduce((accum, current) => accum + current.amount, 0);
 
-  const {button, icon, badge} = styles;
+  const {button, icon, badge, bump } = styles;
+
+  // 담기 버튼을 누를 때 마다 장바구니 버튼 애니메이션 활성화
+  useEffect(() => {
+    if (cartItems.length === 0) return;
+
+    // console.log('useEffect 실행');
+    setIsBump(true);
+
+    // 애니메이션 실행 후 클래스 제거
+    setTimeout(() => {
+      setIsBump(false);
+    }, 300);
+  },[cartItems]);
 
   return (
-    <button className={button} onClick={onShow}>
+    <button className={`${button} ${isBump ? bump : undefined}`} onClick={onShow}>
       <span className={icon}>
         <CartIcon />
       </span>
       <span>My Cart</span>
-      <span className={badge}>{totalSelect}</span>
+      <span className={badge}>{numberOfCart}</span>
     </button>
   );
 };
