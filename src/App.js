9bf3988ec5a20f285ab43780c0779785976ab2af
yocong <rnfaos77@naver.com>
2024-07-03 09:30:18 +0900

    # 라우팅 복습 (실습)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import Home from './components/RouteExample/pages/Home';
-import Products from './components/RouteExample/pages/Products';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import RootLayout from './components/RouteExample/layout/RootLayout';
 import ErrorPage from './components/RouteExample/pages/ErrorPage';
-import ProductDetail from './components/RouteExample/pages/ProductDetail';
+import Events from './components/RouteExample/pages/Events';
+import EventDetail from './components/RouteExample/pages/EventDetail';
 
 // 라우터 설정
 const router = createBrowserRouter([
 
+  // :prodId - ':' 붙으면 변동값
   {
     path: '/',
     element: <RootLayout />,
     errorElement: <ErrorPage />,
-    children: [
+    children: [ // 중첩 라우팅
       { index: true, element: <Home /> },
-      { path: 'products', element: <Products /> },
-      { path: 'products/:prodId/page/:pageNo', element: <ProductDetail /> },
+      { path: 'events', element: <Events /> },
+      { path: 'events/:eventId', element: <EventDetail /> },
     ]
   },
-
 ]);
 
 const App = () => {
