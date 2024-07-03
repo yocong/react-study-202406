1ee004afef25371acf2b5981f0117149817a776a
yocong <rnfaos77@naver.com>
2024-07-03 12:04:15 +0900

    # 에러페이지 커스텀 메시지 처리

--- src/components/RouteExample/pages/ErrorPage.js
+++ src/components/RouteExample/pages/ErrorPage.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import MainNavigation from '../layout/MainNavigation';
+import { useRouteError } from 'react-router-dom';
+import EventsNavigation from '../layout/EventNavigation';
 
 const ErrorPage = () => {
+
+  // 에러가 발생했을 때 throw에 전달한 객체를 읽는 방법
+  const error = useRouteError();
+
+  // json에 있는 것을 화면에 렌더링해주기 위해 파싱
+  let errorMessage;
+
+  if (error.status === 400) {
+    errorMessage = JSON.parse(error.data).message;
+    // console.log(errorMessage);
+  }
+  if (error.status === 404) {
+    errorMessage = '페이지를 찾을 수 없습니다. URL을 확인하세요!'
+  }
+
   return (
     <>
       <MainNavigation />
+      <EventsNavigation />
       <main>
-        <h1>페이지를 찾을 수 없습니다!</h1>
-        <p>요청하신 페이지의 주소를 다시 확인해주세요.</p>
+        <h1>에러가 발생했습니다!</h1>
+        <p>{errorMessage}</p>
       </main>
     </>
   );
--- src/components/RouteExample/pages/Events.js
+++ src/components/RouteExample/pages/Events.js
@@ -9,6 +9,7 @@ const Events = () => {
   // loader가 리턴한 데이터 받아오기
   const eventList = useLoaderData();
 
+  console.log('event page rendering!');
   return (
     <>
       <h1>Events Page</h1>
@@ -22,14 +23,17 @@ export default Events
 // loader를 app.js로부터 아웃소싱
 export const loader = async () => {
 
-  // 이 페이지가 열릴 때 자동으로 트리거되어 호출되는 함수
-  // 이 함수에는 페이지가 열리자마자 해야할 일을 적을 수 있음
-  const response = await fetch('http://localhost:8282/events');
-  // const jsonData = await response.json();
+  const response = await fetch('http://localhost:8282/events?sort=title');
 
-  // loader가 리턴한 데이터는 loader를 선언한 컴포넌트와 그 하위 컴포넌트에서
-  // 언제든 불러서 사용할 수 있다.
-
-  // loader에서 fetch의 결과를 바로 리턴하면 알아서 json을 추출해줌
-  return response;
-}
+  if (!response.ok) {
+    const errorText = await response.text();
+    throw new Response(
+      JSON.stringify({ message: errorText }),
+      {
+        status: response.status
+      }
+    ); // error message
+  }
+  console.log('loader call!');
+  return response; // ok일 경우 events[]
+};
